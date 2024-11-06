230009bfdc87ac3a558bb13de3af041f33d2b933
Sam Bell <sambo@asab.us>
2024-11-05 21:27:12 -0500

    initial port from arduino to pi pico

---
+++ include/global.h
@@ -0,0 +1,7 @@
+#ifndef GLOBAL_H_MARK
+    #define GLOBAL_H_MARK 1
+
+    unsigned long msSinceBoot();
+
+
+#endif
---
+++ src/global.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/i2c.h"
+#include "hardware/dma.h"
+#include "hardware/pio.h"
+#include "hardware/interp.h"
+#include "hardware/timer.h"
+#include "hardware/watchdog.h"
+#include "hardware/clocks.h"
+#include "pico/cyw43_arch.h"
+#include "hardware/uart.h"
+
+#include "include/global.h"
+
+
+unsigned long msSinceBoot()
+{
+    return to_ms_since_boot(get_absolute_time());
+};
